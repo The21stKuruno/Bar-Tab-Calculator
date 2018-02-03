cea1df5cfcfacdc94be09205282a48f0c97d9107
iamcrisaian <The21stKuruno@yahoo.com>
2018-02-04 01:17:35 +0900

    done project

---
+++ run.py
@@ -0,0 +1,11 @@
+from space.bartab import Tab
+
+table_one = Tab()
+
+table_one.add('wine')
+table_one.add('beer')
+table_one.add('soft_drink')
+table_one.add('chicken')
+table_one.add('beef')
+
+table_one.print_bill(10, 10)
---
+++ try.py
@@ -0,0 +1,14 @@
+
+total = 0
+dict = {
+    'apple' : 10,
+    'banana' : 11
+}
+
+
+def add(a, b):
+    total = a + b
+    print(total)
+
+
+add(dict['apple'], dict['banana'])
